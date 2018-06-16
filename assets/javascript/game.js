e897532f3ac15bd1cb8c824d873389c8748626f0
Colby Schumm <coolbreeze@Colbys-MacBook-Air.local>
2018-06-16 09:52:23 -0600

    Be Vader

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,40 @@
+
+// all of my character objects
 var vader = {
-    hitPoints: 1450,
+    hitPoints: 450,
     attkStr: 30,
     defStr: 28,
-    id: "vader"
+    id: "vader",
+    maxHitpoints: 450,
+    box: $("#vader"),
+    text: $("#vaderhp")
 }
 var fett = {
     hitPoints: 115,
     attkStr: 25,
     defStr: 20,
-    id: "fett"
+    id: "fett",
+    maxHitpoints: 115,
+    box: $("#fett"),
+    text: $("#fetthp")
 }
 var solo = {
     hitPoints: 120,
     attkStr: 28,
     defStr: 19,
-    id: "solo"
+    id: "solo",
+    maxHitpoints: 120,
+    box: $("#solo"),
+    text: $("#solohp")
 }
 var luke = {
     hitPoints: 130,
     attkStr: 29,
     defStr: 22,
-    id: "luke"
+    id: "luke",
+    maxHitpoints: 130,
+    box: $("#luke"),
+    text: $("#lukehp")
 }
 var characterObject = {
     "vader": vader,
@@ -28,150 +42,123 @@ var characterObject = {
     "solo": solo,
     "luke": luke
 };
-document.getElementById("vaderhp").innerHTML = "HP: " + vader.hitPoints;
-document.getElementById("fetthp").innerHTML = "HP: " + fett.hitPoints;
-document.getElementById("solohp").innerHTML = "HP: " + solo.hitPoints;
-document.getElementById("lukehp").innerHTML = "HP: " + luke.hitPoints;
-console.log(selectCharacter);
-
-
 var fightObject = {
     "player": null,
     "opponent": null
 }
-var selectArea = document.getElementById("divCharSelection");
-// var selectArea = $("#divCharSelection");
-var opsForArea = document.getElementById("divOpsFor");
-// var opsForArea = $ ("#divOpsFor");
-var oppArea = document.getElementById("computer");
-// var oppAera = $("divOppenent");
-var playArea = document.getElementById("player");
-var deadArea = document.getElementById("divDeadOps");
-// var deadArea = $("divDeadOps");
 
-// javascript
+var selectArea = $("#divCharSelection");
+var opsForArea = $("#divOpsFor");
+var oppArea = $("#computer");
+var playArea = $("#player");
+var deadArea = $("#divDeadOps");
+var playerText = $("#playertext");
+var opponentText = $("#opponenttext");
+// function that runs the damage and records
+var init = function () {
+    for (let key in characterObject) {
+        characterObject[key].hitPoints = characterObject[key].maxHitpoints;
+        characterObject[key].text.html("hp: " + characterObject[key].maxHitpoints);
+        characterObject[key].box.on("click", selectCharacter);
+    }
+}
+// selects my character and moves the none selected cahracters to the defend area
 var selectCharacter = function (e) {
     let obj = e.target == "div" ? e.target : e.target.parentNode;
-    console.log(e)
-
-    obj.onclick = null;
+    console.log(obj);
+    $(obj).off("click");
     fightObject["player"] = characterObject[obj.id];
-    console.log(fightObject["player"]);
-    playArea.appendChild(obj);
-    while (selectArea.childNodes.length > 0) {
-        let curobj = selectArea.childNodes[0];
-        if (curobj.nodeName == "DIV") {
-            curobj.onclick = selectOpponent;
+    playArea.append(obj);
+    for (let key in characterObject) {
+        if (characterObject[key].id !== obj.id) {
+            opsForArea.append(characterObject[key].box);
+            characterObject[key].box.off("click").on("click", selectOpponent);
         }
-        opsForArea.appendChild(curobj);
     }
 }
+// player chooses who to fight
 var selectOpponent = function (e) {
-    if (oppArea.querySelectorAll("div").length == 0) {
-        let obj = e.target == "div" ? e.target : e.target.parentNode;
+    if (fightObject["opponent"] == null) {
+        let obj = e.target == "div" ? e.target : e.target.parentNode
+        console.log(obj);
         fightObject["opponent"] = characterObject[obj.id];
-        console.log(fightObject["opponent"]);
-        obj.onclick = returnOppenent;
-        oppArea.appendChild(obj);
+        $(obj).off("click").on("click", returnOppenent);
+        // obj.onclick = returnOppenent;
+        oppArea.append(obj);
     }
 }
+// if opponent is to tough switches out opponent
 var returnOppenent = function (e) {
     let obj = e.target == "div" ? e.target : e.target.parentNode;
     fightObject["opponent"] = null;
-    obj.onclick = selectOpponent;
-    opsForArea.appendChild(obj);
+    $(obj).off("click").on("click", selectOpponent);
+    // obj.onclick = selectOpponent;
+    opsForArea.append(obj);
 }
 var fight = function () {
+    // player needs to pick  fighter
     if (fightObject["player"] == null) {
-        alert("Pick A Player")
+        alert("Pick A Player");
+        return false;
     }
+    // payerneeds to choose an opponent
     if (fightObject["opponent"] == null) {
-        alert("Choose Your Victim")
+        alert("Choose Your Victim");
+        return false;
     }
     // creates the random number of player attack vs the opponent def
-    var attk = Math.floor(Math.random() * fightObject["player"]["attkStr"]);
-    console.log("player attack", attk);
-    var def = Math.floor(Math.random() * fightObject["opponent"]["defStr"]);
-    console.log("opp def", def);
-    // builds the act of the player attack being > or < the opponent deffense
-    var playerAttak;
-    playerAttak = (attk - def);
-    if (playerAttak < 0) {
-        playerAttak = 0;
-        document.getElementById("playertext").innerHTML = "your attack was blocked";
+    var playerResult = fightResult(fightObject["player"], fightObject["opponent"]);
+    if (playerResult == 0) {
+        playerText.html("your attack was blocked");
     } else {
-        document.getElementById("playertext").innerHTML = "you attacked with " + playerAttak + " damage";
+        playerText.html("you attacked with " + playerResult + " damage");
     }
-
-
-
-
-    fightObject["opponent"]["hitPoints"] -= playerAttak;
+    // records the damage and dispaly it on the screen for the player
+    fightObject["opponent"]["hitPoints"] -= playerResult;
     console.log(fightObject["opponent"]["hitPoints"], "subtract opp hitpont");
-    document.getElementById(fightObject["opponent"]["id"] + "hp").innerHTML = "HP: " + fightObject["opponent"]["hitPoints"];
+    fightObject["opponent"].text.html("HP: " + fightObject["opponent"]["hitPoints"]);
     if (fightObject["opponent"]["hitPoints"] <= 0) {
         // get opponent death here
-        deadArea.appendChild(document.getElementById(fightObject["opponent"]["id"]));
+        deadArea.append(fightObject["opponent"].box);
         fightObject["opponent"] = null;
-        document.getElementById("opponenttext").innerHTML = "you win this round";
-        if (opsForArea.querySelectorAll("div").length == 0) {
-            document.getElementById("playertext").innerHTML = "YOU ARE THE STRONGEST WARRIOR IN THE GALAXY";
+        opponentText.html("you win this round");
+        if ($(opsForArea).children("div").length == 0) {
+            playerText.html("YOU ARE THE STRONGEST WARRIOR IN THE GALAXY");
         }
-
-
         return true;
-
     }
-    var oppAttk = Math.floor(Math.random() * fightObject["opponent"]["attkStr"]);
-    var oppDef = Math.floor(Math.random() * fightObject["player"]["defStr"]);
-    console.log(oppAttk, "oppattk");
-    console.log(oppDef, "player def");
-    var opponentAttack;
-    opponentAttack = (oppAttk - oppDef);
-    if (opponentAttack < 0) {
-        opponentAttack = 0;
-        document.getElementById("opponenttext").innerHTML = "you blocked the opponent attack";
+    // records the damage and dispaly it on the screen for the opponent
+    var opponentResult = fightResult(fightObject["opponent"], fightObject["player"]);
+    if (opponentResult == 0) {
+        opponentText.html("you blocked the opponent attack");
     } else {
-        document.getElementById("opponenttext").innerHTML = "opponent attacked you with " + opponentAttack + " damage";
+        opponentText.html("opponent attacked you with " + opponentResult + " damage");
     }
-    console.log(opponentAttack, "opp attk");
-
-    fightObject["player"]["hitPoints"] -= opponentAttack;
-    console.log(fightObject["player"]["hitPoints"], "subtract player hit points");
-    fightObject["player"]["hitPoints"]
-    document.getElementById(fightObject["player"]["id"] + "hp").innerHTML = "HP: " + fightObject["player"]["hitPoints"];
+    fightObject["player"]["hitPoints"] -= opponentResult;
+    fightObject["player"].text.html("HP: " + fightObject["player"]["hitPoints"]);
     if (fightObject["player"]["hitPoints"] <= 0) {
         // get opponent death here
-        deadArea.appendChild(document.getElementById(fightObject["player"]["id"]));
+        deadArea.append(fightObject["player"].box);
         fightObject["player"] = null;
-        document.getElementById("playertext").innerHTML = "you LOSE this round";
+        playerText.html("you LOSE this round");
         return false;
     }
 }
+var fightResult = function (attacker, defender) {
+    var oppAttk = Math.floor(Math.random() * attacker["attkStr"]);
+    var oppDef = Math.floor(Math.random() * defender["defStr"]);
+    var result = oppAttk - oppDef;
+    return result > 0 ? result : 0;
+}
+// resets the game
 var reset = function () {
-    selectArea.appendChild(document.getElementById("vader"));
-    selectArea.appendChild(document.getElementById("fett"));
-    selectArea.appendChild(document.getElementById("solo"));
-    selectArea.appendChild(document.getElementById("luke"));
-    document.getElementById("vader").onclick = selectCharacter;
-    document.getElementById("fett").onclick = selectCharacter;
-    document.getElementById("solo").onclick = selectCharacter;
-    document.getElementById("luke").onclick = selectCharacter;
-    characterObject["vader"]["hitPoints"] = 145;
-    characterObject["fett"]["hitPoints"] = 115;
-    characterObject["solo"]["hitPoints"] = 120;
-    characterObject["luke"]["hitPoints"] = 130;
-    document.getElementById("vaderhp").innerHTML = "HP: " + vader.hitPoints;
-    document.getElementById("fetthp").innerHTML = "HP: " + fett.hitPoints;
-    document.getElementById("solohp").innerHTML = "HP: " + solo.hitPoints;
-    document.getElementById("lukehp").innerHTML = "HP: " + luke.hitPoints;
+    for (let key in characterObject) {
+        selectArea.append(characterObject[key].box);
+    }
+    init();
+
+
 
 }
-document.getElementById("vader").onclick = selectCharacter;
-document.getElementById("fett").onclick = selectCharacter;
-document.getElementById("solo").onclick = selectCharacter;
-document.getElementById("luke").onclick = selectCharacter;
 
-
-
-// subtracts the difference of the player attack vs the opp def from player or opponents hit ponts
+init();
